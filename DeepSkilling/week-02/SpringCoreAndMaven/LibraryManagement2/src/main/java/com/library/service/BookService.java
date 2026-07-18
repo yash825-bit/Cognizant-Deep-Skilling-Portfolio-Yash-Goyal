fc22cbb1f15e117c494b65a9e92dc7bbd569d4a2
yash825-bit <goyal.yash18335@gmail.com>
2026-07-18 18:04:12 +0530

    feat: completed exercise06 of spring core and maven module moved from xml based configuration to annotation based configuration

--- DeepSkilling/week-02/SpringCoreAndMaven/LibraryManagement2/src/main/java/com/library/service/BookService.java
+++ DeepSkilling/week-02/SpringCoreAndMaven/LibraryManagement2/src/main/java/com/library/service/BookService.java
@@ -1,18 +1,19 @@
 package com.library.service;
 
 import com.library.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookService {
 
+    @Autowired
     private BookRepository bookRepository;
-
-    public void setBookRepository(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
-    }
 
     public void displayBooks() {
 
         System.out.println("Fetching books...");
+
         bookRepository.displayBooks();
     }
 }
