507f738b06386cefe2087f2ef88bf5bb1e8f2c0a
yash825-bit <goyal.yash18335@gmail.com>
2026-07-26 22:46:25 +0530

    feat: completed left over hands on of spring rest

--- DeepSkilling/week-03/SpringRESTusingSpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ DeepSkilling/week-03/SpringRESTusingSpringBoot3/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -4,19 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(SpringLearnApplication.class);
 
+	public static void displayCountry() {
+
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("country.xml");
+
+		Country country = context.getBean("country", Country.class);
+
+		System.out.println(country);
+
+		context.close();
+	}
+
 	public static void main(String[] args) {
 
 		logger.info("main method started");
+		displayCountry();
 
-		SpringApplication.run(SpringLearnApplication.class, args);
-
-		logger.info("spring boot application started");
 	}
 
 }
